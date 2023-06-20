b432240f1ee093d17465942df06a52b7157fca48
defaltcyber <harimezakariae@gmail.com>
2023-06-20 17:59:35 +0100

    Revert "dndn"
    This reverts commit 24f51a2825f631cd26088ab1b7d8a3dfb45ae1a9.

--- CarDealerWeb-Django/cars/models.py
+++ CarDealerWeb-Django/cars/models.py
@@ -44,7 +44,7 @@ class Car(models.Model):
     car_photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
-    features = MultiSelectField(choices=features_choices, max_choices=30)
+    features = MultiSelectField(choices=features_choices)
     body_style = models.CharField(max_length=100)
     engine = models.CharField(max_length=100)
     transmission = models.CharField(max_length=100)
@@ -58,5 +58,75 @@ class Car(models.Model):
 
     def __str__(self):
         return self.car_title
-    #ndndn
 
+
+car_datalist=[ {
+    "car_title": "2023 Lamborghini Aventador SVJ",
+    "color": "Orange",
+    "model": "Aventador SVJ",
+    "year": 2023,
+    "price": 500000,
+    "description": "<p>The Lamborghini Aventador SVJ is a limited-production supercar manufactured by the Italian automotive company Lamborghini. It is the most powerful version of the Aventador and is designed for high-performance track driving.</p>\r\n\r\n<p>The Aventador SVJ is equipped with a naturally aspirated 6.5-liter V12 engine that produces an astonishing 759 horsepower, allowing it to accelerate from 0 to 60 mph in just 2.8 seconds. It features advanced aerodynamics, including an active rear wing and aero vectoring, which enhance its performance and handling.</p>\r\n\r\n<p>Inside the Aventador SVJ, you'll find a luxurious and sporty interior with premium materials and modern technology. It offers a comfortable seating arrangement for two passengers and includes features such as a touchscreen infotainment system, premium audio system, and carbon fiber accents.</p>",
+    "car_photo": "photos/2023/05/30/th-41096962.jpg",
+    "car_photo_1": "photos/2023/05/30/th-2552590400.jpg",
+    "car_photo_2": "photos/2023/05/30/th-3738237238.jpg",
+    "features": "Adaptive Suspension, Carbon Ceramic Brakes, Launch Control, GPS Navigation, Apple CarPlay, Android Auto, Lane Departure Warning, Reverse Camera",
+    "body_style": "Coupe",
+    "engine": "6.5L V12",
+    "transmission": "Automatic",
+    "interior": "Black",
+    "doors": "2",
+    "passengers": 2,
+    "vin_no": "ZXY9876543210",
+    "fuel_type": "Petrol",
+    "is_featured": True,
+    "created_date": datetime.now()
+},
+{
+    "car_title": "Lamborghini Revuelto",
+    "color": "Red",
+    "model": "Revuelto",
+    "year": 2023,
+    "price": 600000,
+    "description": "<p>The Lamborghini Revuelto is a luxury sports car manufactured by Lamborghini. It features a sleek design and powerful performance, making it a true masterpiece of engineering.</p>",
+    "car_photo": "photos/2023/05/30/th-842608322.jpg",
+    "car_photo_1": "photos/2023/05/30/th-2288692250.jpg",
+    "car_photo_2": "photos/2023/05/30/th-3826454088.jpg",
+    "features": "Adaptive Suspension, Carbon Ceramic Brakes, Launch Control, GPS Navigation, Apple CarPlay, Android Auto, Lane Departure Warning, Reverse Camera",
+    "body_style": "Coupe",
+    "engine": "6.8L V12",
+    "transmission": "Automatic",
+    "interior": "Black",
+    "doors": "2",
+    "passengers": 2,
+    "vin_no": "ABC1234567890",
+    "fuel_type": "Petrol",
+    "is_featured": True,
+    "created_date": datetime.now()
+},
+{
+    "car_title": "Dodge Hennessey Venom F5",
+    "color": "Yellow",
+    "model": "Hennessey Venom F5",
+    "year": 2023,
+    "price": 1800000,
+    "description": "<p>The Dodge Hennessey Venom F5 is a high-performance supercar built by Hennessey Performance Engineering based on the Dodge platform. It is a true speed demon with mind-blowing acceleration and top speed.</p>",
+    "car_photo": "photos/2023/05/30/th-3193446790.jpg",
+    "car_photo_1": "photos/2023/05/30/th-3294177422.jpg",
+    "car_photo_2": "photos/2023/05/30/th-2405398302.jpg",
+    "features": ["Carbon Ceramic Brakes", "Launch Control", "GPS Navigation", "Apple CarPlay", "Android Auto"],
+    "body_style": "Coupe",
+    "engine": "8.8L V8",
+    "transmission": "Automatic",
+    "interior": "Black",
+    "doors": "2",
+    "passengers": 2,
+    "vin_no": "ABC1234567890",
+    "fuel_type": "Petrol",
+    "is_featured": True,
+    "created_date": datetime.now()
+}
+]
+for car_data in car_datalist:
+    car = Car(**car_data)
+    car.save()
