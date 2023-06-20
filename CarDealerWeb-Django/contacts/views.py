853e83bdc55b39549985daaca16b679e81fb6ce1
defaltcyber <harimezakariae@gmail.com>
2023-06-20 17:26:39 +0100

    view change

--- CarDealerWeb-Django/contacts/views.py
+++ CarDealerWeb-Django/contacts/views.py
@@ -4,7 +4,6 @@ from .models import Contact
 from django.core.mail import send_mail
 from django.contrib.auth.models import User
 
-
 # Create your views here.
 def inquiry(request):
     if request.method == 'POST':
@@ -18,28 +17,37 @@ def inquiry(request):
         phone = request.POST['phone']
         message = request.POST['message']
 
-
         if request.user.is_authenticated:
-             user_id = request.user.id
-             has_contacted = Contact.objects.all().filter(car_id=car_id, user_id=user_id)
-             if has_contacted:
-                 messages.error(request, 'You have already made an inquiry about this car. Please wait until we get back to you.')
+            user_id = request.user.id
+            has_contacted = Contact.objects.filter(car_id=car_id, user_id=user_id).exists()
+            if has_contacted:
+                messages.error(request, 'You have already made an inquiry about this car. Please wait until we get back to you.')
+                return redirect('/cars/'+car_id)
+
+        contact = Contact(
+            car_id=car_id,
+            car_title=car_title,
+            user_id=user_id,
+            first_name=first_name,
+            last_name=last_name,
+            customer_need=customer_need,
+            email=email,
+            phone=phone,
+            message=message
+        )
+        contact.save()
+
+        admin_info = User.objects.get(is_superuser=True)
+        admin_email = admin_info.email
+        send_mail(
+            'New Car Inquiry',
+            'You have a new inquiry for the car ' + car_title + '. Please login to your admin panel for more info.',
+            'harimezakariae@gmail.com',
+            [admin_email],
+            fail_silently=False,
+        )
+
+        messages.success(request, 'Your request has been submitted. We will get back to you shortly.')
         return redirect('/cars/'+car_id)
 
-    contact = Contact(car_id=car_id, car_title=car_title, user_id=user_id,
-    first_name=first_name, last_name=last_name, customer_need=customer_need,
-     email=email, phone=phone, message=message)
-
-    admin_info = User.objects.get(is_superuser=True)
-    admin_email = admin_info.email
-    send_mail(
-                 'New Car Inquiry',
-                 'You have a new inquiry for the car ' + car_title + '. Please login to your admin panel for more info.',
-                 'harimezakariae@gmail.com',
-                 [admin_email],
-                 fail_silently=False,
-             )
-
-    contact.save()
-    messages.success(request, 'Your request has been submitted, we will get back to you shortly.')
-    return redirect('/cars/'+car_id)
+    return redirect('/')  # Add a fallback redirect if the request method is not POST
